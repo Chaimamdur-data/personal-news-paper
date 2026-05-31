979045490cdfc0397b48ca6821eb6f76d4a6b504
CMamdur <mchaitanya@gmail.com>
2026-05-31 16:52:26 -0700

    Update llm_score.py

--- llm_score.py
+++ llm_score.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 def summarize_news(news_text):
     prompt = f"""
